43f9a21aaf3bb31b5a70377059549263b137ab08
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-10-02 22:14:48 +0530

    added connect-wallet api

---
+++ src/app/api/connect-wallet/route.ts
@@ -0,0 +1,42 @@
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req : NextRequest) {
+    try {
+        
+        const { publicKey } = await req.json();
+
+        if(!publicKey) {
+            return NextResponse.json({
+                message : "Public key is missing!"
+            }, { status : 411})
+        }
+
+        const user = await prisma.user.findFirst({
+            where : {
+                walletAddress : publicKey.toString()
+            }
+        })
+
+        if(!user) {
+            await prisma.user.create({
+                data : {
+                    walletAddress : publicKey.toString(),
+                    isVerified : false,
+                }
+            })
+        }
+
+        return NextResponse.json({
+           user
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            message : "Internal Server Error"
+        }, { 
+            status : 500
+        })
+    }
+
+}
--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -2,17 +2,46 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "sonner";
+import { Button } from "./ui/button";
 
 export default function Appbar() {
   const { publicKey } = useWallet();
+  const [isVerified, setIsVerified] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      try {
+        const res = await axios.post("/api/connect-wallet", {
+          publicKey,
+        });
+        setIsVerified(res.data.user.isVerified);
+      } catch (error: any) {
+        toast.error(error.response.data.message ?? error.message);
+      }
+    };
+    if (publicKey) {
+      connectWallet();
+    }
+  }, [publicKey]);
 
   return (
     <div className="flex items-center p-4 bg-black justify-between">
-      <div className="font-semibold text-lg text-white cursor-pointer">
-        Solana<span className="text-yellow-400">Launchpad</span>
+      <div className="font-semibold text-xl text-white cursor-pointer">
+        Token<span className="text-yellow-400">Forge</span>
       </div>
-      <div>{publicKey ? <WalletMultiButton /> : <WalletMultiButton />}</div>
+      <div className="flex items-center gap-4">
+        {publicKey ? <WalletMultiButton /> : <WalletMultiButton />}
+        <div>
+          {publicKey && isVerified === false && (
+            <Button className="px-6 py-2 bg-purple-500 hover:bg-purple-600">
+              Verify Wallet
+            </Button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
