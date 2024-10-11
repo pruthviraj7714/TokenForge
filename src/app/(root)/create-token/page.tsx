e3d225366a0da448705f7c46dab2c48ebed3d9a4
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-10-11 23:35:34 +0530

    logic fixes

--- src/app/(root)/create-token/page.tsx
+++ src/app/(root)/create-token/page.tsx
@@ -14,31 +14,26 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import {
-  ASSOCIATED_TOKEN_PROGRAM_ID,
   createAssociatedTokenAccountInstruction,
   createInitializeMetadataPointerInstruction,
   createInitializeMintInstruction,
   createMintToInstruction,
   ExtensionType,
-  getAssociatedTokenAddress,
   getAssociatedTokenAddressSync,
   getMintLen,
   LENGTH_SIZE,
   TOKEN_2022_PROGRAM_ID,
   TYPE_SIZE,
 } from "@solana/spl-token";
-import {
-  Keypair,
-  PublicKey,
-  SystemProgram,
-  Transaction,
-} from "@solana/web3.js";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 import { Button } from "@/components/ui/button";
 import { createInitializeInstruction, pack } from "@solana/spl-token-metadata";
 import { toast } from "sonner";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { LucideLoader2 } from "lucide-react";
+import { isTokenAlreadyExist, isUserVerified } from "@/actions/actions";
 
 const formSchema = z.object({
   image: z.string().url({ message: "Image must be valid URL" }),
@@ -51,25 +46,18 @@ const formSchema = z.object({
     .min(2, { message: "Symbol must be at least of 2 charcters" })
     .max(6, { message: "Symbol should be no longer than 6 characters" }),
   decimal: z
-    .number()
+    .number({message : "Please Put the Decimals"})
     .min(0, { message: "The Decimals Cannot be less than 0" })
     .max(9, { message: "Decimals Cannot be more than 9" }),
   supply: z.number().min(1, { message: "Put the Amount to mint" }),
-  recipientAddress: z.string(),
-  mintAmount: z.number(),
+  mintAmount: z.number({message : "Please Put the Amount to mint"}),
 });
 
 export default function CreateTokenPage() {
-  const { publicKey, sendTransaction, signTransaction } = useWallet();
+  const { publicKey, sendTransaction } = useWallet();
   const { connection } = useConnection();
-  const [createAtaDialogOpen, setCreateAtaDialogOpen] = useState(false);
   const router = useRouter();
   const [sending, setSending] = useState(false);
-  const [recipientAddress, setRecipientAddress] = useState("");
-  const [mintAmount, setMintAmount] = useState<number>(0);
-  const [mintTokenPublicKey, setMintTokenPublicKey] = useState<string>("");
-  const [mintKeyPair, setMintKeyPair] = useState<Keypair | null>(null);
-  const [minting, setMinting] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -79,7 +67,6 @@ export default function CreateTokenPage() {
       symbol: "",
       decimal: 0,
       supply: 0,
-      recipientAddress: "",
       mintAmount: 0,
     },
   });
@@ -89,15 +76,37 @@ export default function CreateTokenPage() {
       toast.error("Wallet is not connected");
       return;
     }
-
     setSending(true);
+
+    const isVerified = await isUserVerified(publicKey?.toString());
+
+    if (!isVerified) {
+      toast.error("Please Verify Your Wallet First", {
+        position: "top-center",
+      });
+      setSending(false);
+      return;
+    }
+
+    const isTokenAlreadyExists = await isTokenAlreadyExist(
+      values.name,
+      values.symbol
+    );
+
+    if (isTokenAlreadyExists) {
+      toast.error("Token with same name or symbol already exists.", {
+        description: "Please Change Name/Symbol",
+        position: "top-center",
+      });
+      setSending(false);
+      return;
+    }
+
     let isError = false;
     let tx;
     let metaData;
     try {
       const mintKeyPair = Keypair.generate();
-      setMintKeyPair(mintKeyPair);
-      setMintTokenPublicKey(mintKeyPair.publicKey.toString());
       const transaction = new Transaction();
 
       metaData = {
@@ -113,35 +122,6 @@ export default function CreateTokenPage() {
 
       const lamports = await connection.getMinimumBalanceForRentExemption(
         mintLen + metadataLen
-      );
-
-      const ata = getAssociatedTokenAddressSync(
-        mintKeyPair.publicKey,
-        new PublicKey(values.recipientAddress),
-        false,
-        TOKEN_2022_PROGRAM_ID
-      );
-
-      console.log("Generated ATA:", ata);
-
-      const createAtaInstruction = createAssociatedTokenAccountInstruction(
-        publicKey,
-        ata,
-        new PublicKey(values.recipientAddress),
-        mintKeyPair.publicKey,
-        TOKEN_2022_PROGRAM_ID,
-        ASSOCIATED_TOKEN_PROGRAM_ID
-      );
-
-      const mintAmountInBaseUnits =
-        mintAmount * Math.pow(10, values.decimal);
-      const mintToInstruction = createMintToInstruction(
-        mintKeyPair.publicKey,
-        ata,
-        publicKey,
-        mintAmountInBaseUnits,
-        [],
-        TOKEN_2022_PROGRAM_ID
       );
 
       transaction.add(
@@ -174,9 +154,7 @@ export default function CreateTokenPage() {
           updateAuthority: publicKey,
           mint: mintKeyPair.publicKey,
           metadata: mintKeyPair.publicKey,
-        }),
-        createAtaInstruction,
-        mintToInstruction
+        })
       );
 
       transaction.feePayer = publicKey;
@@ -186,12 +164,48 @@ export default function CreateTokenPage() {
 
       transaction.partialSign(mintKeyPair);
       tx = await sendTransaction(transaction, connection);
-      console.log(tx);
+
+      const associatedToken = getAssociatedTokenAddressSync(
+        mintKeyPair.publicKey,
+        publicKey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      console.log(associatedToken.toBase58());
+
+      const transaction2 = new Transaction().add(
+        createAssociatedTokenAccountInstruction(
+          publicKey,
+          associatedToken,
+          publicKey,
+          mintKeyPair.publicKey,
+          TOKEN_2022_PROGRAM_ID
+        )
+      );
+
+      await sendTransaction(transaction2, connection);
+
+      const transaction3 = new Transaction().add(
+        createMintToInstruction(
+          mintKeyPair.publicKey,
+          associatedToken,
+          publicKey,
+          values.mintAmount * Math.pow(10, values.decimal),
+          [],
+          TOKEN_2022_PROGRAM_ID
+        )
+      );
+
+      await sendTransaction(transaction3, connection);
+
+      console.log("Minted!");
 
       toast.success(`${metaData.name} tokens successfully minted!`, {
-        description: `Check it on ${recipientAddress}.`,
+        description: `Check it in your wallet.`,
         position: "top-center",
       });
+      form.reset();
     } catch (error: any) {
       isError = true;
       toast.error(error.message);
@@ -223,153 +237,153 @@ export default function CreateTokenPage() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-br from-purple-900 via-slate-800 to-black">
-      <Card className="w-[550px] p-8 bg-opacity-90 bg-gradient-to-b from-gray-800 to-gray-900 shadow-lg rounded-lg border border-gray-700">
+      <Card className="w-[450px] md:w-[550px] lg:w-[970px] my-2 p-8 bg-opacity-90 bg-gradient-to-b from-gray-800 to-gray-900 shadow-lg rounded-lg border border-gray-700">
         <CardTitle className="my-6 text-2xl font-semibold text-white">
           Create your own token
         </CardTitle>
         <CardContent>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-gray-300">Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
-                        placeholder="Enter name for your token"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="symbol"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-gray-300">Symbol</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
-                        placeholder="Enter Symbol for your token"
-                        {...field}
-                      />
-                    </FormControl>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                <div>
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-gray-300">Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
+                            placeholder="Enter name for your token"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="symbol"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-gray-300">Symbol</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
+                            placeholder="Enter Symbol for your token"
+                            {...field}
+                          />
+                        </FormControl>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="image"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-gray-300">Image</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
-                        placeholder="Enter image url for your token"
-                        {...field}
-                      />
-                    </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="image"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-gray-300">Image</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
+                            placeholder="Enter image url for your token"
+                            {...field}
+                          />
+                        </FormControl>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="decimal"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-gray-300">Decimal</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
-                        type="number"
-                        placeholder="Enter the decimal you want for your token"
-                        {...field}
-                        onChange={(e) => field.onChange(Number(e.target.value))}
-                      />
-                    </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div>
+                  <FormField
+                    control={form.control}
+                    name="decimal"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-gray-300">Decimal</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
+                            type="number"
+                            placeholder="Enter the decimal you want for your token"
+                            {...field}
+                            onChange={(e) =>
+                              field.onChange(Number(e.target.value))
+                            }
+                          />
+                        </FormControl>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="supply"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-gray-300">Supply</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
-                        type="number"
-                        placeholder="Enter the amount of tokens to mint"
-                        {...field}
-                        onChange={(e) => field.onChange(Number(e.target.value))}
-                      />
-                    </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="supply"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-gray-300">Supply</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
+                            type="number"
+                            placeholder="Enter the amount of tokens to mint"
+                            {...field}
+                            onChange={(e) =>
+                              field.onChange(Number(e.target.value))
+                            }
+                          />
+                        </FormControl>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="recipientAddress"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-gray-300">
-                      Recipient Address
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
-                        placeholder="Enter the address for which tokens to mint for that account"
-                        {...field}
-                      />
-                    </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="mintAmount"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-gray-300">
+                          Mint Amouont
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
+                            type="number"
+                            placeholder="Enter the amount of tokens to mint"
+                            {...field}
+                            onChange={(e) =>
+                              field.onChange(Number(e.target.value))
+                            }
+                          />
+                        </FormControl>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="mintAmount"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-gray-300">
-                      Mint Amouont
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="bg-gray-700 text-white border-none focus:ring-2 focus:ring-purple-500"
-                        type="number"
-                        placeholder="Enter the amount of tokens to mint"
-                        {...field}
-                        onChange={(e) => field.onChange(Number(e.target.value))}
-                      />
-                    </FormControl>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
               <Button
+                disabled={sending}
                 className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 rounded-lg transition duration-300"
                 type="submit"
               >
-                Submit
+                {sending ? (
+                  <div className="flex items-center gap-1.5">
+                    <LucideLoader2 className="animate-spin" />
+                    Creating Token...
+                  </div>
+                ) : (
+                  "Submit"
+                )}
               </Button>
             </form>
           </Form>
