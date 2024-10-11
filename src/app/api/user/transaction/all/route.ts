52d7057c4de5da752c2a7085f01f8d748e17a02e
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-10-11 23:33:35 +0530

    added route for all txn

---
+++ src/app/api/user/transaction/all/route.ts
@@ -0,0 +1,56 @@
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const walletAddress = req.nextUrl.searchParams.get("walletAddress");
+
+    if (!walletAddress) {
+      return NextResponse.json(
+        {
+          message: "Wallet Address Missing!",
+        },
+        { status: 400 }
+      );
+    }
+
+    console.log(walletAddress);
+
+    const user = await prisma.user.findFirst({
+      where: {
+        walletAddress : walletAddress.toString(),
+      },
+      include: {
+        transactions: {
+          orderBy : {
+            createdAt : "desc"
+          },
+          include : {
+            token : true
+          }
+        },
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "User not found!",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      transactions: user.transactions,
+      profilePhoto : user.profilePhoto
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
